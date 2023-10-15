0f4932e4cd07f99f2855c6daf2f2c1ef3b69cb85
alibusidan <ali.busidan@hotmail.com>
2023-10-15 15:58:24 +0300

    initial commit

---
+++ JScript/script.js
@@ -0,0 +1,32 @@
+const nav = document.querySelector('.nav')
+window.addEventListener('scroll', fixNav)
+
+function fixNav() {
+	if (window.scrollY > nav.offsetHeight + 150) {
+		nav.classList.add('active')
+	} else {
+		nav.classList.remove('active')
+	}
+}
+
+// const buttons = document.querySelectorAll('.sign-in')
+
+// buttons.forEach(button => {
+// 	button.addEventListener('click', function(e)  {
+// 		const x = e.clientX
+// 		const y = e.clientY
+
+// 		const buttonTop = e.target.offsetTop
+// 		const buttonLeft = e.target.offsetLeft
+
+// 		const xInside = x - buttonLeft
+// 		const yInside = y - buttonTop
+
+// 		const circle = document.createElement('span')
+// 		circle.classList.add('circle')
+// 		circle.style.top = yInside + 'px'
+// 		circle.style.left = xInside + 'px'
+
+// 		this.appendChild(circle)
+// 	})
+// })
